c7264e28e3957e91d6a8089a4d244a2ac29ba53a
Dipak <codecraftr26@gmail.com>
2025-09-09 16:26:06 +0530

    Initial Spring Boot GST Invoice Backend project

---
+++ src/main/java/com/dipak/gst/gstinvoicebackend/GstInvoiceBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dipak.gst.gstinvoicebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstInvoiceBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GstInvoiceBackendApplication.class, args);
+	}
+
+}
